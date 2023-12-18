0f9fd8ea320901d7250529fc82f9e41f0e75073a
ChayoBee <cacao34@hotmail.com>
2023-12-17 23:35:28 -0300

    Commit 2

--- assets/JavaScript/lista.js
+++ assets/JavaScript/lista.js
@@ -1,83 +1,64 @@
 const titulo = document.getElementById('inputTarea');
-const exp = document.getElementById('inputDesc');
 const btnAdd = document.getElementById('btnAdd');
 const total = document.getElementById('spanTotal');
 const hecho = document.getElementById('spanHecho');
 const lista = document.getElementById('tareaList');
 
+// Tareas impresas
+
 let tareas = [
-    {   id: 11,
-        name: 'Stickers', 
-        done: false, 
-        desc: 'Imprimir los stickers'
-    },
-    {   id: 12,
-        name: 'Chapas',
-        done: false,
-        desc: 'Armar las chapitas'
-    },
-    {   id: 13,
-        name: 'Llaveros',
-        done: false,
-        desc: 'Ir a buscar los llaveros'
-    }
+    { id: 1, name: ' Imprimir los stickers', done: false},
+    { id: 2, name: ' Armar las chapitas', done: false},
+    { id: 3, name: ' Ir a buscar los llaveros', done: false}
 ];
 
-const renderLi = (name, desc, done) => (
-    `<li>
-      <span>${name}</span>
-      <span>${desc}</span>
-      <button type="button" onclick="borrar('${name}')">Eliminar</button>
-      <button type="button" onclick="check('${name}')">
-        ${done ? "✔️" : "❌"}
-      </button>
-    </li>`
-  );
-  
-  const addTarea = () => {
-    if (titulo.value != "" && exp.value !=""){ 
-        tareas.push( {
-            name: titulo.value,
-            done: false,
-            desc: exp.value,
-        });
-    };
-
-    renderTarea();
-  };
-  
-  btnAdd.addEventListener('click', addTarea);
-  
-  const borrar = (tareaAEliminar) => {
-    tareas = tareas.filter((tarea) => tarea.name != tareaAEliminar);
-
-    renderTarea();
-  };
-  
-  const check = (tareaAMarcar) => {
-    const tareaCheck = tareas.filter((tarea) => tarea.name == tareaAMarcar);
-    const tareaNoCheck = tareas.filter((tarea) => tarea.name != tareaAMarcar);
-    if (tareacheck.length > 0){ 
-      tareacheck[0].done = !tareacheck[0].done;
-      tareas = [...tareaCheck, ...tareaNoCheck];
-    };
-
-    renderTarea();
-  };
-  
-  
-  const renderTarea = () => {
-    tareas = tareas.sort((a, b) => a.name.localeCompare(b.name));
+const contador = () => {
     lista.innerHTML = ""
-    for (tarea of tareas){
-      lista.innerHTML += renderLi(
-        tareas[tarea].name,
-        tareas[tarea].desc,
-        tareas[tarea].done
-       );
-    };
     total.innerHTML = tareas.length;
     hecho.innerHTML = tareas.filter((tarea) => tarea.done == true).length
   };
   
-  renderTarea();
+  contador(tareas); 
+
+
+
+/*const contadores = () => {
+    lista.innerHTML = "";
+    const totalTareas = tareas.length;
+    const totalHecho = tareas.filter(t => t.done == true).length;
+    total.textContent = `${totalTareas}`;
+    hecho.textContent = `${totalHecho}`;
+};
+contadores(tareas); */
+
+function renderTareas(tareas) {
+    let html = '';
+    for (let tarea of tareas) {
+        html += `
+        <div>
+        <li>${tarea.id} ${tarea.name}
+        <input type="checkbox" ${tarea.done ? 'checked' : ''} onchange = "checkea(${tarea.id})">
+        <div onclick = 'borrar(${tarea.id})'>❌</div>
+        </li>
+        </div>`
+    };
+    lista.innerHTML = html;
+};
+renderTareas(tareas);
+
+btnAdd.addEventListener("click", () => {
+    const nameTarea = titulo.value;
+    const nuevaTarea = {id: tareas.length +1, name:nameTarea, done: false};
+
+    tareas.push(nuevaTarea);
+    //titulo.value= '';
+
+    
+   renderTareas(tareas);
+});
+
+function borrar(id){
+    const index = tareas.findIndex((elemento) => elemento.id == id);
+    tareas.splice(index, 1);
+    renderTareas(tareas);
+};
---
+++ assets/JavaScript/lista2.js
@@ -0,0 +1,84 @@
+const titulo = document.getElementById('inputTarea');
+const exp = document.getElementById('inputDesc');
+const btnAdd = document.getElementById('btnAdd');
+const total = document.getElementById('spanTotal');
+const hecho = document.getElementById('spanHecho');
+const lista = document.getElementById('tareaList');
+
+
+let tareas = [
+    {   id: 1,
+        name: 'Stickers', 
+        done: false, 
+        desc: 'Imprimir los stickers'
+    },
+    {   id: 2,
+        name: 'Chapas',
+        done: false,
+        desc: 'Armar las chapitas'
+    },
+    {   id: 3,
+        name: 'Llaveros',
+        done: false,
+        desc: 'Ir a buscar los llaveros'
+    }
+];
+
+const renderLi = (name, desc, done) => (
+    `<li>
+      <span>${name}</span>
+      <span>${desc}</span>
+      <button type="button" onclick="borrar('${name}')">Eliminar</button>
+      <button type="button" onclick="check('${name}')">
+        ${done ? "✔️" : "❌"}
+      </button>
+    </li>`
+  );
+  
+  const addTarea = () => {
+    if (titulo.value != "" && exp.value !=""){ 
+        tareas.push( {
+            name: titulo.value,
+            done: false,
+            desc: exp.value,
+        });
+    };
+
+    renderTarea();
+  };
+  
+  btnAdd.addEventListener('click', addTarea);
+  
+  const borrar = (tareaAEliminar) => {
+    tareas = tareas.filter((tarea) => tarea.name != tareaAEliminar);
+
+    renderTarea();
+  };
+  
+  const check = (tareaAMarcar) => {
+    const tareaCheck = tareas.filter((tarea) => tarea.name == tareaAMarcar);
+    const tareaNoCheck = tareas.filter((tarea) => tarea.name != tareaAMarcar);
+    if (tareacheck.length > 0){ 
+      tareacheck[0].done = !tareacheck[0].done;
+      tareas = [...tareaCheck, ...tareaNoCheck];
+    };
+
+    renderTarea();
+  };
+  
+  
+  const renderTarea = () => {
+    tareas = tareas.sort((a, b) => a.name.localeCompare(b.name));
+    lista.innerHTML = ""
+    for (tarea of tareas){
+      lista.innerHTML += renderLi(
+        tareas[tarea].name,
+        tareas[tarea].desc,
+        tareas[tarea].done
+       );
+    };
+    total.innerHTML = tareas.length;
+    hecho.innerHTML = tareas.filter((tarea) => tarea.done == true).length
+  };
+  
+  renderTarea();
